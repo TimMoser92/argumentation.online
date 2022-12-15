e3f9ae59ecd8b9b8a1c69efe487556dfac77277a
Tim Moser <tim.moser@zweitag.de>
2022-12-15 15:18:21 +0100

    tmp(chore): update build to beta 10

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -50,162 +50,162 @@ export default [
   },
   {
     path: '/en/reconstructions/tags/adjunktiver-syllogismus',
-    component: ComponentCreator('/en/reconstructions/tags/adjunktiver-syllogismus','6c2'),
+    component: ComponentCreator('/en/reconstructions/tags/adjunktiver-syllogismus','dbd'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/anselm',
-    component: ComponentCreator('/en/reconstructions/tags/anselm','045'),
+    component: ComponentCreator('/en/reconstructions/tags/anselm','b81'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/antinomie',
-    component: ComponentCreator('/en/reconstructions/tags/antinomie','7e5'),
+    component: ComponentCreator('/en/reconstructions/tags/antinomie','b63'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/aristoteles',
-    component: ComponentCreator('/en/reconstructions/tags/aristoteles','0d3'),
+    component: ComponentCreator('/en/reconstructions/tags/aristoteles','14f'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/ausschlussprinzip',
-    component: ComponentCreator('/en/reconstructions/tags/ausschlussprinzip','cd7'),
+    component: ComponentCreator('/en/reconstructions/tags/ausschlussprinzip','542'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/das-ich',
-    component: ComponentCreator('/en/reconstructions/tags/das-ich','9de'),
+    component: ComponentCreator('/en/reconstructions/tags/das-ich','7ec'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/disjunktiver-syllogismus',
-    component: ComponentCreator('/en/reconstructions/tags/disjunktiver-syllogismus','c91'),
+    component: ComponentCreator('/en/reconstructions/tags/disjunktiver-syllogismus','96a'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/existenz',
-    component: ComponentCreator('/en/reconstructions/tags/existenz','cd8'),
+    component: ComponentCreator('/en/reconstructions/tags/existenz','ddf'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/gott',
-    component: ComponentCreator('/en/reconstructions/tags/gott','634'),
+    component: ComponentCreator('/en/reconstructions/tags/gott','6c3'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/gottesbeweis',
-    component: ComponentCreator('/en/reconstructions/tags/gottesbeweis','dff'),
+    component: ComponentCreator('/en/reconstructions/tags/gottesbeweis','a8c'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/hedonismus',
-    component: ComponentCreator('/en/reconstructions/tags/hedonismus','f88'),
+    component: ComponentCreator('/en/reconstructions/tags/hedonismus','685'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/hume',
-    component: ComponentCreator('/en/reconstructions/tags/hume','6d9'),
+    component: ComponentCreator('/en/reconstructions/tags/hume','63d'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/informell',
-    component: ComponentCreator('/en/reconstructions/tags/informell','4d5'),
+    component: ComponentCreator('/en/reconstructions/tags/informell','612'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/jackson',
-    component: ComponentCreator('/en/reconstructions/tags/jackson','e77'),
+    component: ComponentCreator('/en/reconstructions/tags/jackson','a5e'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/kant',
-    component: ComponentCreator('/en/reconstructions/tags/kant','ed1'),
+    component: ComponentCreator('/en/reconstructions/tags/kant','e6a'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/lebewesen',
-    component: ComponentCreator('/en/reconstructions/tags/lebewesen','2de'),
+    component: ComponentCreator('/en/reconstructions/tags/lebewesen','6d5'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/leibniz',
-    component: ComponentCreator('/en/reconstructions/tags/leibniz','637'),
+    component: ComponentCreator('/en/reconstructions/tags/leibniz','2e9'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/logik',
-    component: ComponentCreator('/en/reconstructions/tags/logik','43c'),
+    component: ComponentCreator('/en/reconstructions/tags/logik','489'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/mary',
-    component: ComponentCreator('/en/reconstructions/tags/mary','148'),
+    component: ComponentCreator('/en/reconstructions/tags/mary','d37'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/maschinen',
-    component: ComponentCreator('/en/reconstructions/tags/maschinen','93b'),
+    component: ComponentCreator('/en/reconstructions/tags/maschinen','b32'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/modus-tollendo-ponens',
-    component: ComponentCreator('/en/reconstructions/tags/modus-tollendo-ponens','35a'),
+    component: ComponentCreator('/en/reconstructions/tags/modus-tollendo-ponens','be9'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/modus-tollendo-tollens',
-    component: ComponentCreator('/en/reconstructions/tags/modus-tollendo-tollens','2ff'),
+    component: ComponentCreator('/en/reconstructions/tags/modus-tollendo-tollens','fb3'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/modus-tollens',
-    component: ComponentCreator('/en/reconstructions/tags/modus-tollens','29b'),
+    component: ComponentCreator('/en/reconstructions/tags/modus-tollens','508'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/moralphilosophie',
-    component: ComponentCreator('/en/reconstructions/tags/moralphilosophie','a97'),
+    component: ComponentCreator('/en/reconstructions/tags/moralphilosophie','2bf'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/nietzsche',
-    component: ComponentCreator('/en/reconstructions/tags/nietzsche','d09'),
+    component: ComponentCreator('/en/reconstructions/tags/nietzsche','e4e'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/nozick',
-    component: ComponentCreator('/en/reconstructions/tags/nozick','3e3'),
+    component: ComponentCreator('/en/reconstructions/tags/nozick','19e'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/philosophie',
-    component: ComponentCreator('/en/reconstructions/tags/philosophie','140'),
+    component: ComponentCreator('/en/reconstructions/tags/philosophie','e4d'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/philosophie-des-geistes',
-    component: ComponentCreator('/en/reconstructions/tags/philosophie-des-geistes','093'),
+    component: ComponentCreator('/en/reconstructions/tags/philosophie-des-geistes','b32'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/physikalismus',
-    component: ComponentCreator('/en/reconstructions/tags/physikalismus','351'),
+    component: ComponentCreator('/en/reconstructions/tags/physikalismus','47e'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/raum',
-    component: ComponentCreator('/en/reconstructions/tags/raum','a83'),
+    component: ComponentCreator('/en/reconstructions/tags/raum','116'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/satz-vom-widerspruch',
-    component: ComponentCreator('/en/reconstructions/tags/satz-vom-widerspruch','85b'),
+    component: ComponentCreator('/en/reconstructions/tags/satz-vom-widerspruch','10b'),
     exact: true
   },
   {
     path: '/en/reconstructions/tags/zeit',
-    component: ComponentCreator('/en/reconstructions/tags/zeit','542'),
+    component: ComponentCreator('/en/reconstructions/tags/zeit','e0e'),
     exact: true
   },
   {
@@ -240,7 +240,7 @@ export default [
   },
   {
     path: '/en/search',
-    component: ComponentCreator('/en/search','bd2'),
+    component: ComponentCreator('/en/search','730'),
     exact: true
   },
   {
